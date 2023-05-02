305dd6e2137a01ddee80c886de2184ea547d6df2
coker2019 <olabisi2k11@gmail.com>
2023-05-02 11:49:58 +0100

    Add awesome-books

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
-const bookList = [
-    {
-    bookTitle: 'African queen'
-   
-    },
-    {
-      bookTitle: 'African queen'
-     
-      },
-      {
-        bookTitle: 'African queen'
-       
-        },
-        {
-          bookTitle: 'African queen'
-          
-         
-          },
-  ];
+const bookList = localStorage.getItem('book') ? JSON.parse(localStorage.getItem('book')) : [];
+
+console.log(bookList);
+
+const addBook = (e) => {
+    e.preventDefault();
+
+    let book = {
+        id: Date.now(),
+        title: document.getElementById('title').value,
+        author: document.getElementById('author').value,
+    }
+    if (book.id && book.title && book.author) {
+        bookList.push(book);
+    }
+    document.querySelector('form').reset();
+
+    localStorage.setItem('book', JSON.stringify(bookList));
+    location.reload();
+}
+
+bookList.forEach((element, e) => {
+    const allBooks = document.querySelector('.all-books');
+    const book = document.createElement('div');
+    book.classList = 'book-style';
+
+    book.innerHTML = `
+    
+    <h2 class="title">${bookList[e].title}</h2>
+    <p class="author">${bookList[e].author}</p>
+    <button class="remove">Remove</button>
+    <hr />
+    `;
+    allBooks.append(book);
+    
+});
+
+function activateDelete() {
+    let remove = document.querySelectorAll('.remove');
+    console.log(remove);
+    remove.forEach((btn, i) => {
+        btn.addEventListener('click', () => { deleteBook(i) })
+               
+    })
+}
+
+//filter function
+
